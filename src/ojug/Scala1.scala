2c005d9a50e728bd6868d018a5da33f0c2ed239b
Stephen Haberman <stephen@exigencecorp.com>
2011-09-24 23:17:00 -0500

    Add comments for var example.

--- src/ojug/Scala1.scala
+++ src/ojug/Scala1.scala
@@ -1,6 +1,10 @@
 package ojug
 
 class Scala1 {
+  // looks like a public field, but ends up as:
+  // private String name
+  // public String name() { return name; }
+  // public void name_$eq(String name) { this.name = name; }
   var name = "foo"
 
   def foo(name: String) = {
@@ -10,6 +14,8 @@ class Scala1 {
 object Scala1 {
   def main(args: Array[String]) {
     val one = new Scala1
+    // looks like a field reference, but really calls
+    // one.name_$eq("blah")
     one.name = "blah"
     println(one.name)
   }
